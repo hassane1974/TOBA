be44108dd2947316d39f7a75b028c8bf0c4a4204
hassane <hassane@10.230.160.190>
2016-10-31 12:59:14 -0400

    Merge origin/master
    Conflicts:
    	build/web/New_customer.html
    	build/web/WEB-INF/classes/business/Customer.class
    	build/web/WEB-INF/classes/toba/NewCustomerServlet.class
    	nbproject/private/private.xml
    	src/java/business/Customer.java
    	src/java/toba/NewCustomerServlet.java
    	web/New_customer.html
    	web/Sucess.jsp

--- src/java/toba/NewCustomerServlet.java
+++ src/java/toba/NewCustomerServlet.java
@@ -5,7 +5,6 @@ package toba;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -17,48 +16,56 @@ import business.Customer;
 
 @WebServlet(urlPatterns = {"/NewCustomerServlet"})
 public class NewCustomerServlet extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-        String url = "/Sucess.jsp";
-      
- 
+        String url = "/index.html";
+
         // get current action
         String action = request.getParameter("action");
         if (action == null) {
-            action = "New_customer.html";  // default action
+            action = "add";  // default action
         }
-
-        if (action.equals("Submit")) {   
-              String firstName = request.getParameter("firstName");
+        if (action.equals("add")) {
+            String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
             String email = request.getParameter("email");
-             String  Phone = request.getParameter("Phone");
-            String  Address= request.getParameter("Address");
-            String  City = request.getParameter("City");
-             String State = request.getParameter("State");
+            String Phone = request.getParameter("Phone");
+            String Address = request.getParameter("Address");
+            String City = request.getParameter("City");
+            String State = request.getParameter("State");
             String Zipcode = request.getParameter("Zipcode");
-            
 
             // store data in User object
-          
-            Customer customer = new Customer(firstName, lastName, email,Phone , Address, City, State, Zipcode);
+            Customer customer = new Customer(firstName, lastName, email, Phone, Address, City, State, Zipcode);
             // get parameters from the request
-           request.setAttribute("Customer", customer);
-           url = "/Sucess.jsp";}
-              
-          
+            String message;
+            if (firstName.equals("")||lastName.equals("")||email.equals("")||Address.equals("")||City.equals("")||State.equals("")||Zipcode.equals("")) {
+            // forward to the view to get missing parameters
+            message="Please fill out all boxes";
+            url = "/New_customer.jsp";
+        } else 
+             message="";
+            url = "/Sucess.jsp";
+        
+            request.setAttribute("Customer", customer);
+            //url = "/Sucess.jsp";
+        }
+
         // forward request and response objects to specified URL
         getServletContext()
-            .getRequestDispatcher(url)
-            .forward(request, response);
-    }    
-    
+                .getRequestDispatcher(url)
+                .forward(request, response);
+
+        
+    }
+
     @Override
-    protected void doGet(HttpServletRequest request, 
-                          HttpServletResponse response) 
-                          throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
         doPost(request, response);
-    }    
+    }
 }
