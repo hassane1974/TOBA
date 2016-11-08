c6a751776cbb8e35a502d31b1dafde6271294d54
hassane <hassane@10.230.160.190>
2016-11-08 15:26:32 -0500

    ok

--- src/java/toba/LoginServlet.java
+++ src/java/toba/LoginServlet.java
@@ -1,7 +1,7 @@
 package toba;
 
-
 import business.Login;
+import business.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -16,12 +17,13 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
-  @Override
-    protected void doPost(HttpServletRequest request, 
-                          HttpServletResponse response) 
-                          throws ServletException, IOException {
 
-        String url = "/Login.html";
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url = "/Login.jsp";
 
         // get current action
         String action = request.getParameter("action");
@@ -31,45 +33,57 @@ public class LoginServlet extends HttpServlet {
 
         // perform action and set URL to appropriate page
         if (action.equals("Login")) {
-            url = "/Login.html";    // the "join" page
-        }
-        else if (action.equals("add")) {                
+            url = "/Login.jsp";    // the "join" page
+        } else if (action.equals("add")) {
             // get parameters from the request
             String username = request.getParameter("Username");
-            
-            String password = request.getParameter("Password");
-            
 
+            String password = request.getParameter("Password");
+            String message = null;
+            if (username.equals("") || password.equals("")) {
+                // forward to the view to get missing parameters
+                message = "*********Please fill out all boxes**************";
+                url = "/Login.jsp";}
+            else{  
+                message = "";       
+            } 
+                
+ 
+            
             // store data in User object and save User object in database
             Login login = new Login(username, password);
-           
-          
+             User user;
+             HttpSession session = request.getSession();
+            user = (User) session.getAttribute("user");
+             
+            session.setAttribute("user", user);
             // set User object in request object and set URL
             request.setAttribute("Login", login);
-           // url = "/index.html";   // 
-        
-          if (username.equals("jsmith@toba.com")&& password.equals("letmein")){
-              
-              url = "/Account_activity.html";
-          }
-            else {
-             
-              url = "/displayinfo.jsp";
-              
-          }
-        
+            request.setAttribute("message", message);
+            // url = "/index.html";   // 
+
+            if (username.equals(user.userName)&& password.equals(user.passWord)) {
+
+                url = "/Account_activity.jsp";
+            } else {
+                
+                url = "/displayinfo.jsp";
+
+            }
+
         }
-        
+
         // forward request and response objects to specified URL
         getServletContext()
-            .getRequestDispatcher(url)
-            .forward(request, response);
-    }    
-    
+                .getRequestDispatcher(url)
+                .forward(request, response);
+
+    }
+
     @Override
-    protected void doGet(HttpServletRequest request, 
-                          HttpServletResponse response) 
-                          throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
         doPost(request, response);
-    }    
+    }
 }
